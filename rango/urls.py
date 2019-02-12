d6e286243c981caf564b0b793891e9b731235cb1
Filip <2421756s@student.gla.ac.uk>
2019-02-12 15:47:42 +0000

    Chap 7 mid

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from rango import views
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+	url(r'^$', views.index, name='index'),
+	url(r'^add_category/$', views.add_category, name='add_category'),
 		views.show_category, name='show_category'),
 ]
